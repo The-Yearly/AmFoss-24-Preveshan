5baee4327fd5b514c0d3eae7d94b1e4283665d86
The-Yearly <theyearly@NotAtAllShady.NotAtAllShady>
2024-09-14 04:03:05 +0530

    Added C++

---
+++ #Task-03/C++/Sub-Task3/SubTask3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+        int n;
+        cout<<"n: ";
+        cin>>n;
+        if(n%2!=0){
+            int c=n-(n/2)-1;
+            int m=1;
+            int s1=-1;
+            int s2=1;
+            for(int g=0;g<n;g++){
+                if(c!=0){
+                    for(int j=0;j<c;j++){
+                        cout<<" ";
+                    }
+                }
+                for (int j=0;j<m;j++) {
+                    cout<<"*";
+                }
+                cout<<endl;
+                m+=s2*2;
+                c+= s1*1;
+                if(c==0){
+                    s1=1;
+                }if(m>n){
+                    s2=-1;
+                    m=n-2;
+                }
+            }
+        }else{
+            cout<<"Enter odd number" << endl;
+        }
+    return 0;
+}
